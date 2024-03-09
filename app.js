46d505f52704d3e60fa1b63eebc6afcdb63c0ab0
Tixxy-A <anujraut7221@gmail.com>
2024-03-09 18:58:52 +0530

    cors

--- app.js
+++ app.js
@@ -19,8 +19,22 @@ app.use(cookieparser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 
-app.use(cors());
-mongoose.connect(process.env.MONGOURL);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  });
+  mongoose.connect(process.env.MONGOURL);
 
 
 app.post('/register', async (req, res) => {
